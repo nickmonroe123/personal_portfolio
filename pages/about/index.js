b3a667c848246dde934bc1511d2cf11502dc17f3
m30509 <nm309u@att.com>
2023-12-22 15:12:19 -0600

    for deployment

--- pages/about/index.js
+++ pages/about/index.js
@@ -158,7 +158,19 @@ const About = () => {
                         >{item.title}</div>
                     })}
                 </div>
-
+                <div className='py-2 xl:py-6 flex flex-col gap-y-2 xl:gap-y-4 items-center xl:items-start'>
+                    {aboutData[index].info.map((item, itemIndex)=> {
+                        return
+                            <div className='flex-1 flex flex-col md:flex-row max-w-max gap-x-2
+                            items-center text-white/60'>
+                                {/* title */}
+                                <div className='font-light mb-2 md:mb-0'>{item.title}</div>
+                                <div className='hidden md:flex'>-</div>
+                                <div>{item.stage}</div>
+ 
+                            </div>
+                    })}
+                </div>
             </motion.div>
         </div>
     </div>
