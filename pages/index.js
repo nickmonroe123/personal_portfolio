12d9c0a13dcf788624b16041662c67aa4f1b405d
m30509 <nm309u@att.com>
2023-12-22 14:12:24 -0600

    for deployment

--- pages/index.js
+++ pages/index.js
@@ -19,30 +19,62 @@ const Home = () => {
              <div className='w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10'>
                 <div className='text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto'>
                     {/* title */}
-                    <h1 className='h1'>
+                    <motion.h1
+                        variants={fadeIn('down', 0.2)}
+                        initial="hidden"
+                        animate="show"
+                        exit="hidden"
+                        className='h1'>
                         Transforming Ideas <br /> Into{' '}
                         <span className='text-accent'>
                             Digital Reality
                         </span>
-                    </h1>
+                    </motion.h1>
                     {/* subtitle */}
-                    <p className='max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16'>
+                    <motion.p
+                        variants={fadeIn('down', 0.3)}
+                        initial="hidden"
+                        animate="show"
+                        exit="hidden"
+                        className='max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16'>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
                         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
                         veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
                         commodo consequat.
-                    </p>
+                    </motion.p>
                     {/* btn */}
                     <div className='flex justify-center xl:hidden relative'>
                         <ProjectsBtn />
                     </div>
-                    <div>
+                    <motion.div
+                        variants={fadeIn('down', 0.4)}
+                        initial="hidden"
+                        animate="show"
+                        exit="hidden"
+                        className='hidden xl:flex'
+                        >
                         <ProjectsBtn />
-                    </div>
+                    </motion.div>
                 </div>
              </div>
              {/* image */}
-             <div>image</div>
+             <div className='w-[1200px] h-full absolute right-0 bottom-0'>
+                {/* bg image */}
+                <div className='bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full
+                absolute mix-blend-color-dodge translate-z-0'></div>
+                {/* particles */}
+                <ParticlesContainer />
+                {/* avatar img */}
+                <motion.div
+                    variants={fadeIn('up', 0.2)}
+                    initial="hidden"
+                    animate="show"
+                    transition={{duration: 1, ease: 'easeInOut'}}
+                    exit="hidden" className='w-full h-full max-w-[737px] max-h-[678px] absolute -bottom-32
+                    lg:bottom-0 lg:right-[8%]'>
+                    <Avatar />
+                </motion.div>
+             </div>
          </div>
          );
 };
