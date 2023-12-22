4b8c010237f5860267306b0cab7f242cdec4caf8
m30509 <nm309u@att.com>
2023-12-22 15:14:28 -0600

    for deployment

--- pages/about/index.js
+++ pages/about/index.js
@@ -161,7 +161,7 @@ const About = () => {
                 <div className='py-2 xl:py-6 flex flex-col gap-y-2 xl:gap-y-4 items-center xl:items-start'>
                     {aboutData[index].info.map((item, itemIndex)=> {
                         return
-                            <div key={itemIndex} className='flex-1 flex flex-col md:flex-row max-w-max gap-x-2
+                            <div key={item.id} className='flex-1 flex flex-col md:flex-row max-w-max gap-x-2
                             items-center text-white/60'>
                                 {/* title */}
                                 <div className='font-light mb-2 md:mb-0'>{item.title}</div>
