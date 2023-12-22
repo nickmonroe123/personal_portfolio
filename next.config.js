087cbfbd12b0f33e2ad535e38f230efd73f3cee1
m30509 <nm309u@att.com>
2023-12-22 15:47:43 -0600

    for deployment

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  images.unoptimized = true
+  swcMinify: true
 }
 
+images.unoptimized = true
 module.exports = nextConfig
 
 module.exports = {
