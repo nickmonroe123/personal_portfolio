9c897a4b196b3149f0982170c3e668789156b927
m30509 <nm309u@att.com>
2024-01-05 16:06:55 -0600

    adding api

--- pages/_app.js
+++ pages/_app.js
@@ -12,12 +12,21 @@ import {useRouter} from 'next/router';
 // framer motion
 import {AnimatePresence, motion} from 'framer-motion';
 
+const env = process.env.NODE_ENV;
+var image_name = '/favicon.ico';
+if(env == "development"){
+    image_name = '/favicon.ico';
+}
+else if (env == "production"){
+    image_name = 'favicon.ico';
+}
+
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
   return (
   <Layout>
     <Head>
-      <link rel="shortcut icon" href="/favicon.ico" />
+      <link rel="shortcut icon" href=image_name />
     </Head>
     <AnimatePresence mode='wait'>
         <motion.div key={router.route} className='h-full'>
