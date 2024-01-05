0d511279cfa627fdda7068ec0e7795fbd421a791
m30509 <nm309u@att.com>
2024-01-05 11:37:53 -0600

    adding api

--- pages/api/SendEmail.js
+++ pages/api/SendEmail.js
@@ -1,9 +1,10 @@
 import fetch from 'node-fetch';
 
 export default async function handler(req, res) {
+  console.log("ASD123");
   if (req.method === 'GET') {
     const { text } = req.body;
-    console.log("ASD")
+    console.log("ASD");
     const response = await fetch('https://nicks-apis.onrender.com/send_email', {
       method: 'GET',
       headers: {
@@ -11,7 +12,7 @@ export default async function handler(req, res) {
       },
       //body: JSON.stringify({ text }),
     });
-    console.log("ASD2")
+    console.log("ASD2");
 
     console.log(response);
     console.log(response.text);
